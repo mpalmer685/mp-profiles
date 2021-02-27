65cd07d5324878b89656058b94a286b16c4fc2ac
Mike Palmer <mrpalmer685@gmail.com>
2021-02-27 11:32:41 -0500

    Add zsh profile

---
+++ zsh/antigen.sh
@@ -0,0 +1,32 @@
+# Enable Antigen
+source "$(brew --prefix)/share/antigen/antigen.zsh"
+
+antigen use oh-my-zsh
+
+antigen bundle git
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle "MichaelAquilina/zsh-you-should-use"
+
+antigen bundle agkozak/agkozak-zsh-prompt
+
+antigen apply
+
+# Customize prompt
+AGKOZAK_BLANK_LINES=1
+AGKOZAK_COLORS_BRANCH_STATUS=green
+AGKOZAK_PROMPT_DIRTRIM=3
+AGKOZAK_PROMPT_CHAR=( ❯ ❯ ❮ )
+AGKOZAK_USER_HOST_DISPLAY=0
+AGKOZAK_CUSTOM_SYMBOLS=(
+  '%F{yellow}⇣⇡%f'  # Diverged
+  '%F{yellow}⇣%f'   # Behind
+  '%F{yellow}⇡%f'   # Ahead
+  '%F{red}+%f'      # Unstaged
+  '%F{red}x%f'      # Deleted
+  '%F{red}!%f'      # Modified
+  '%F{red}>%f'      # Renamed
+  '%F{red}?%f'      # Untracked
+  '%F{green}S%f'    # Stashed
+)
+AGKOZAK_CUSTOM_RPROMPT='%(3V.%F{yellow}(%6v%f%(7V. ${psvar[7]}.)%F{yellow}%)%f.)'
